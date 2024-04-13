89f16dea8be88ce25624e942d9f1761f078e05d5
AleixFIB <aleix.lopez.padro@estudiantat.upc.edu>
2024-04-13 18:42:40 +0200

    base esquelet feta

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@
 //import './App.css';
 
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Header from './Header';
 import ReportBug from './ReportBug';
@@ -11,21 +11,23 @@ import RequestOrg from './RequestOrg';
 
 function App() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-    </div>
-    /*
     <Router>
-      <div>
-        <Header />
-          <Route exact path="/" component={Home} />
-          <Route path="/report-bug" component={ReportBug} />
-          <Route path="/report-user" component={ReportUser} />
-          <Route path="/request-org" component={RequestOrg} />
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <div style={{ flex: '0 0 200px', backgroundColor: 'lightgray', padding: '20px' }}>
+          {/* Routes inside the left gray space */}
+          <Header />
+        </div>
+        <div style={{ flex: '1', height: '100%', overflowY: 'auto', padding: '20px'  }}>
+          {/* Main content goes here */}
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/report-bug" element={<ReportBug />} />
+            <Route path="/report-user" element={<ReportUser />} />
+            <Route path="/request-org" element={<RequestOrg />} />
+          </Routes>
+        </div>
       </div>
     </Router>
-    */
   );
 }
 
