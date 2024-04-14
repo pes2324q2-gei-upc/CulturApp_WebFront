bac1f5a66d9f92dbbacfdd7619e327b7db90ea1f
AleixFIB <aleix.lopez.padro@estudiantat.upc.edu>
2024-04-14 22:22:02 +0200

    login fet amb css, falta funcionalitat amb api

---
+++ web/src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css'; // Importing the CSS file
+
+
+function Login() {
+  return (
+    <div className="container">
+        <div className="heading">Sign In</div>
+        <form action="" className="form">
+          <input required className="input" type="email" name="email" id="email" placeholder="E-mail" />
+          <input required className="input" type="password" name="password" id="password" placeholder="Password" />
+          <input className="login-button" type="submit" value="Sign In" />
+        </form>
+    </div>
+  );
+}
+
+export default Login;
