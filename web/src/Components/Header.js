e60841aeff36e49c91f5cd6bb0fc96ad5c494fce
carles.lobon <carleslobonquilez@gmail.com>
2024-04-21 13:01:19 +0200

    primer versió menu

--- web/src/Components/Header.js
+++ web/src/Components/Header.js
@@ -2,17 +2,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 //import "../Header.css"
+import "../stylesheet.css"
 
 function Header() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/report-bug">Report Bug</Link></li>
-        <li><Link to="/report-user">Report User</Link></li>
-        <li><Link to="/request-org">Request Org</Link></li>
-      </ul>
-    </nav>
+    <div className="container">
+      <div className="container-menu">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/report-bug">Report Bug</Link></li>
+            <li><Link to="/report-user">Report User</Link></li>
+            <li><Link to="/request-org">Request Org</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="content">
+        
+      </div>
+    </div>
   );
 }
 
